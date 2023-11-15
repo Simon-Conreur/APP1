871638e3e1efa5b381bbab8adeb2c2d034b4d2c4
Simon-Conreur <simon.conreur@student.uclouvain.be>
2023-11-15 12:32:56 +0100

    Modification pendant tp projet S9

--- simulation.py
+++ simulation.py
@@ -17,14 +17,18 @@ def rotation(p, c, angle):
     :param p: Point qui tourne (y,z)
     :param c: Centre de rotation (y, z)
     :param angle: Angle de rotation (radiants)
-    :return: rotation(p)
+    :return: les coordonnées du points p apres rotation de centre c d'angle angle
     """
-    np.array(p)
-    np.array(c)
+    p = np.array(p)
+    c = np.array(c)
     p -= c
+    y_p = p[0]
+    z_p = p[1]
     # matrice de rotation
-    p[0] = cos(angle)*p[0] - sin(angle)*p[1]
-    p[1] = sin(angle)*p[0] + cos(angle)*p[1]
+    p[0] = cos(angle)*y_p - sin(angle)*z_p
+    p[1] = sin(angle)*y_p + cos(angle)*z_p
+
+    p += c
     return [p[0], p[1]]
 
 
@@ -34,7 +38,7 @@ def translation(point, vect):
     Déplace un point
     :param point: point à déplacer
     :param vect: vecteur de déplacement
-    :return: le nouveau point 
+    :return: les coordonnées de point après translation de vecteur vect
     """
     point[0] += vect[0]
     point[1] += vect[1]
@@ -47,7 +51,7 @@ def r1_2_0(point):
     :return: Le point dans r2
     """
     point[0], point[1] = point[0] + v1_2[0], point[1] + v1_2[1]  # translation vers R1
-    rotation(point, [0,0], angl_0)
+    rotation(point, [0,0], angl_0)  # rotation selon l'angle initial
     return point
 
 
@@ -57,7 +61,7 @@ def g_trapeze(theta, l, hc, v):
     :param theta: Angle de rotation
     :param l: largeur de la barge
     :param hc: hauteur de l'eau au niveau du centre de gravité (hc)
-    :param v: composante y du vecteur R1->R2
+    :param v: composante y du vecteur R1->R2 (= v1_2)
     :return: le centre de poussée P
     """
     hl = hc + tan(theta)*(l + (-v))
@@ -73,25 +77,25 @@ def g_trapeze(theta, l, hc, v):
 # /!\ les points sont donnés dans le repère 1
 # constantes en SI ---------------------------------------------------------------->
 #  constantes physiques
-g = 9.81
-rho = 1000
-D = 0  # constante d'ammortissemnt
-I = 1  # Inertie de la structure
+g = 9.81  # accélération due à la gravité de la Terre
+rho = 1000  # masse volumique de l'eau
+D = 1  # constante d'ammortissemnt
 
 # constantes dimensions
 L = 0.60  # Largeur de la barge
-h1 = 0  # hauteur de la barge
-h2 = 0  # hauteur du mas
-d1 = 0  # distance centre de la barge/ centre du mas
+L2 = 0.05  # largeur du mat
+h1 = 0.07  # hauteur de la barge
+h2 = 0.50  # hauteur du mat
+d1 = 0.10  # distance centre de la barge/ centre du mat
 
 #  masses
-m1 = 0  # masse barge
-m2 = 0  # masse mat + 1er bras
-m_charge = 0  # Masse le la charge portée
-m_bras_grap = 0  # Masse bras 2 et grappin
+m1 = 0.700  # masse barge
+m2 = 0.250  # masse mat + 1er bras
+m_charge = 0.200  # Masse le la charge portée
+m_bras_grap = 0.100  # Masse bras 2 et grappin
 m3 = m_charge + m_bras_grap  # masse grappin + 2 eme bras + charge
-m_tot = m1 + m2 + m3  # masse totale de la structure
 # masses relatives
+m_tot = m1 + m2 + m3  # masse totale de la structure
 m_c = m2  # masse charge totale
 m_s = m1  # masse de la stucture
 
@@ -101,14 +105,17 @@ m_s = m1  # masse de la stucture
 hc = m_tot/(L**2*rho)
 
 # centres de gravité en Y initiaux
-G1_0 = []  # centre de gravité de la barge
-G2_0 = []  # centre de gravité du mas
+G1_0 = [0, -hc+h1/2]  # centre de gravité de la barge
+G2_0 = [d1, -hc +h1 + h2/2]  # centre de gravité du mas
 G3_0 = []  # centre de gravité du bras 2, de la charge utile et du grappin
 
 # Vecteur de translation pour passer du repère 1 au repère 2
 # Les points sont calculés pour correspondre au repère 2 apd ici ---------------------------->
-v1_2 = [d1*m2/m_s, 0]
-v2_1 = [-d1*m2/m_s, 0]
+v1_2 = [-d1*m2/m_s, 0] # vecteur 1 vers 2
+v2_1 = [d1*m2/m_s, 0]  # vecteur 2 vers 1
+
+# Inertie de la structure
+I = (m1/12)*(h1**2 + L**2 + (hc - h1/2)**2) + m2*(h2**2 + L2**2 + d1**2 + (hc - h1 - h2/2)**2)
 
 # Centres relatifs
 G_0 = [0, (m1*(-hc+h1/2)+m2*(h1-hc+h2/2))/(m1+m2)]  # centre de gravité /!\ Change enfontction de ce qu'on veut simuler
@@ -116,7 +123,7 @@ G_c_0 = []  # centre de gravité de la charge (ce qui va appliquer un couple san
 P_0 = [G_0[0], -hc/2]  # change en fonction de simulation
 
 # valeur initiales des variables principales
-angl_0 = 0  # angle initiale
+angl_0 = 0  # angle initial
 v_angl_0 = 0  # vitesse angulaire initilale
 hl_0 = hc  # hauteur d'eau à gauche ! dépend de l'angle et de la position de G par rapport aux bords de la barge/!\ à modifier
 hr_0 = hc  # hauteur d'eau à droite
@@ -147,20 +154,16 @@ c_a = np.empty_like(t)  # Couple appliqué par la charge
 c_d = np.empty_like(t)  # Couple d'amortidssenment(v_angl)
 
 # centre de gravité
-G = np.empty_like(t)
-for i in range(len(G)):
-    G[i] = [0,0]  # Remplir la liste de [0,0] : coordonées nulles
+tab = []
+for i in range(len(t)):
+    tab.append([0, 0])  # Remplir la liste de [0,0] : coordonées nulles
+G = np.array(tab)
+
+# centre de poussée rempli de coordonnées nulles
+P = np.array(tab)
+# centre de gravité de la charge rempli de coordonnées nulles
+G_c = np.array(tab)
 print("G initial vaut :", G)
-
-#  Centre de poussée
-P = np.empty_like(t)
-for i in range(len(P)):
-    P[i] = [0,0]  # Remplir la liste de [0,0] : coordonées nulles
-
-# centre de gravité de la charge
-G_c = np.empty_like(t)
-for i in range(len(G_c)):
-    G_c[i] = [0,0]  # Remplir la liste de [0,0] : coordonées nulles
 
 # Déplacement des points initiaux vers le repère
 G_0 = r1_2_0(G_0)
@@ -186,8 +189,8 @@ def simulation():
         c_r[i] = f_p*(P[i][0]-G[i][0])
 
         # vitesse et accélération au temps i+1  avec la méthode d'Euler depuis F = Ia
-        v_angl[i+1] = v_angl[i]*(1-D*dt/I) + (c_r[i] - c_a)*dt/I
-        angl[i+1] = angl[i]+v_angl[i+1]*dt
+        v_angl[i+1] = v_angl[i] + (-D*v_angl[i] + c_r[i] - c_a[i])*dt/I
+        angl[i+1] = angl[i]+v_angl[i]*dt
 
 
 # Representation graphique
